ea5ba7e6f29e4a34fbbb88855c055452fc7e3efc
kumina-dev <ville@kumina.dev>
2024-06-18 18:06:43 +0300

    Add basic routing system and home page template

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+    <footer>
+        <p>&copy; <?= date('Y') ?> My Personal Website</p>
+    </footer>
+</body>
+</html>
---
+++ pages/home.php
@@ -0,0 +1,9 @@
+<?php
+include './includes/header.php';
+?>
+
+<h1>Welcome to the home page</h1>
+<p>This is the home page.</p>
+
+<?php
+include './includes/footer.php';
